38a354d4233e4d29028d11d717f991b695c8d84f
TorreJohnson <torre.johnson@gmail.com>
2018-05-25 12:19:46 -0400

    added seed data

---
+++ client/src/data.js
@@ -0,0 +1,16 @@
+export const data = [
+	{
+		_id: 1,
+		author: "Bryan",
+		text: "Wow, this is neat",
+		updatedAt: new Date(),
+		createdAt: new Date()
+	},
+	{
+		_id: 2,
+		author: "Torre",
+		text: "You're __Right!__",
+		updatedAt: new Date(),
+		createdAt: new Date()
+	}
+];
