d4967486518bfcd688910664a8d388e284bbf6fd
TorreJohnson <torre.johnson@gmail.com>
2018-05-24 21:37:53 -0400

    added jsx to CommentBox and CommentList

---
+++ client/src/CommentList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Comment from "./Comment";
+
+export const CommentList = props => {
+	const commentNodes = props.data.map(comment => (
+		<Comment author={comment.author} key={comment._id} id={comment._id}>
+			{comment.text}
+		</Comment>
+	));
+	return <div>{CommentNodes}</div>;
+};
+
+CommentList.propTypes = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			author: PropTypes.string,
+			id: PropTypes.string,
+			text: PropTypes.string
+		})
+	)
+};
+
+CommentList.defaultProps = {
+	data: []
+};
