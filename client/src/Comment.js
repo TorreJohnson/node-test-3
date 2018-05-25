47a74b568d076ab61c1ef59bbfbac72dbb2008b2
TorreJohnson <torre.johnson@gmail.com>
2018-05-25 12:15:28 -0400

    added Comment

---
+++ client/src/Comment.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
+
+export const Comment = props => (
+	<div className="singleComment">
+		<img
+			alt="user_image"
+			className="userImage"
+			src={`https://picsum.photos/70?random=${props.id}`}
+		/>
+		<div className="textContext">
+			<div className="singleCommentContent">
+				<h3>{props.author}</h3>
+				<ReactMarkdown source={props.children} />
+			</div>
+			<div className="singleCommentButtons" />
+		</div>
+	</div>
+);
+
+Comment.propTypes = {
+	author: PropTypes.string.isRequired,
+	children: PropTypes.string.isRequired,
+	timestamp: PropTypes.string.isRequired
+};
